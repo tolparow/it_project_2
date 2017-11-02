a23fe480731603deabc4dfa848a8aae1aedfb04a
BorisAnimal <iguru98@yandex.ru>
2017-11-02 21:39:28 +0300

    добавил сравнение битов файлов/строк

--- iochannel.py
+++ iochannel.py
@@ -50,11 +50,41 @@ def write_bytes_with_noise(bytes, file_name = "output.txt", noise_level = 0.0):
 def write_string_with_noise(string, file_name = "output.txt", noise_level = 0.0):
     write_to_file(make_some_noise(string, noise_level, is_string=True), file_name)
 
+
+def count_difference_bytes(original, received):
+    assert (len(original) == len(received)), "Different length of original and received"
+    count = 0
+    for i in range(len(original)):
+        b1 = original[i]
+        b2 = received[i]
+        for bit in range(8):
+            if (b1 & 1) != (b2 & 1):
+                count+=1
+            b1 >>= 1
+            b2 >>= 1
+    return count
+
+def count_difference_files(file1, file2):
+    bytes1 = get_bytes(file1, is_file=True)
+    bytes2 = get_bytes(file2, is_file=True)
+    return count_difference_bytes(bytes1, bytes2)
+
+
+# dif = count_difference_files("input.txt", "input2.txt")
+
+
+# dif = 24
+# while dif>=20:
+#     b = b"abc"
+#     dif = count_difference_bytes(b, make_some_noise(b, 0.99))
+#
+# print(str(dif))
+
+
 ##work with string
 # inp = "abc"
 # print("input is >> ", inp)
 # write_string_with_noise(inp, noise_level=0.1)
-
 
 
 # #work with bytes
