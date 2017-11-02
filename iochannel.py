ff88c32563007954cbbf58177f6e480a2331ba66
BorisAnimal <iguru98@yandex.ru>
2017-11-02 21:04:07 +0300

    noise fixed

--- iochannel.py
+++ iochannel.py
@@ -12,8 +12,8 @@ def get_str(file_path):
     with open(file_path, 'r') as f:
         return f.read()
 
-def make_some_noize(bytes, noize_level = 0.0, is_string=False):
-    assert(noize_level >= 0 and noize_level <= 1), ("Wrong value of noize_level! " + str(noize_level) + " not in [0;1]  ")
+def make_some_noise(bytes, noise_level = 0.0, is_string=False):
+    assert(noise_level >= 0 and noise_level <= 1), ("Wrong value of noise_level! " + str(noise_level) + " not in [0;1]  ")
     set_of_bytes = bytearray()
     if is_string:
         set_of_bytes = bytearray(bytes, 'utf-8')
@@ -25,7 +25,7 @@ def make_some_noize(bytes, noize_level = 0.0, is_string=False):
         b = byte
         for i in range(8): #for every bit
             tmp >>= 1
-            if random.random() < noize_level:
+            if random.random() < noise_level:
                 #reverse
                 tmp += 128 * (1 - b & 1)
             else:
@@ -44,20 +44,20 @@ def write_to_file(content, file_name = "output.txt", is_string = False):
             f.write(content)
 
 
-def write_bytes_with_noize(bytes, file_name = "output.txt", noize_level = 0.0):
-    write_to_file(make_some_noize(bytes,noize_level), file_name)
+def write_bytes_with_noise(bytes, file_name = "output.txt", noise_level = 0.0):
+    write_to_file(make_some_noise(bytes,noise_level), file_name)
 
-def write_string_with_noize(string, file_name = "output.txt", noize_level = 0.0):
-    write_to_file(make_some_noize(string, noize_level, is_string=True), file_name)
+def write_string_with_noise(string, file_name = "output.txt", noise_level = 0.0):
+    write_to_file(make_some_noise(string, noise_level, is_string=True), file_name)
 
 ##work with string
 # inp = "abc"
 # print("input is >> ", inp)
-# write_string_with_noize(inp, noize_level=0.1)
+# write_string_with_noise(inp, noise_level=0.1)
 
 
 
 # #work with bytes
 # i = b"abc"
 # print("input is >> ", i)
-# write_bytes_with_noize(i, noize_level=1.0)
+# write_bytes_with_noise(i, noise_level=1.0)
