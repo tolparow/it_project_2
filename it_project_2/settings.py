af4657d6193dba856443b6aef45473557139860c
Tim Tolparov <tolparow@gmail.com>
2017-11-07 12:10:53 +0300

    Static files for admin app collected

--- it_project_2/settings.py
+++ it_project_2/settings.py
@@ -115,6 +115,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# TODO UNCOMMENT STATICFILES_DIRS AND COMMENT STATIC_ROOT FUR USING ON LOCALHOST !!!
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
