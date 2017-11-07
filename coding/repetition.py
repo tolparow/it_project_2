ed960d586fbe79a9faaf0cff3f2777a55aa7c2bb
BorisAnimal <iguru98@yandex.ru>
2017-11-07 20:19:27 +0300

    repetition coding/encoding доделаны

--- coding/repetition.py
+++ coding/repetition.py
@@ -1,9 +1,6 @@
-from noize import iochannel as io
-from bitarray import bitarray
-import binascii
-
 #Repetition algorithm by default 3
 # TODO: do both tests for mult = 3 and mult = 5
+#returns coded bytes
 def code(message, mult = 3, is_string=True):
     bits = ""
     bytes = bytearray()
@@ -20,7 +17,6 @@ def code(message, mult = 3, is_string=True):
     for bit in bits:
         for i in range(mult):
             result += bit
-    print(len(result))
     return bitstring_to_bytes(result)
 
 
@@ -33,12 +29,37 @@ def bitstring_to_bytes(s):
         v >>= 8
     return bytes(b[::-1])
 
-# def encode(message, mult=3):
+#returns encoded bytes
+def encode(message, mult = 3, is_string=True):
+    bits = ''
+    bytes = bytearray()
+    if is_string:
+        bytes = bytearray(message, 'utf8')
+    else:
+        bytes = message[:]
+
+    for st in bytes:
+        tmp = bin(st)[2:]
+        bits += '0' * (8 - len(tmp)) + tmp
+    result = ''
+    for i in range(0, len(bits), mult):
+        count = 0
+        for oft in range(mult):
+            if bits[i + oft] == '1':
+                count += 1
+        if count > mult / 2:
+            result += '1'
+        else:
+            result += '0'
+    return bitstring_to_bytes(result)
+
 
 
 kek = code("kek")
-print(kek)
-io.write_file(kek)
+# print(kek)
+
+decoded = encode(kek, is_string=False)
+print(decoded)
 
 
 # bytes = io.get_bytes("input.txt", is_file=True)
