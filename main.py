e26599fdbcefd1e3fd91c4307ee449a65685a8c1
Nikolay Matyashov <lave0chance@gmail.com>
2017-11-12 11:06:05 +0000

    File creation added, not all final files work right

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import lz
 import sys
 import time
+import rlp
 
-# LZ78 and usage:
-print('Size in bytes before compressing : ' + sys.getsizeof(lz.get_str('sample.txt')).__str__())
+# LZ78 usage:
+
+#file_name = 'gif1.gif'     #1-3
+#file_name = 'tif1.tif'     #1-10
+#file_name = 'bmp5.bmp'     #1-6
+#file_name = 'wav3.wav'     #1-3
+#file_name = 'rtf1.rtf'     #1-3
+#file_name = 'aiff1.aiff'   #1-3
+#file_name = 'txt1.txt'     #1-6
+print('Size in bytes before compressing : ' + sys.getsizeof(lz.get_str(file_name)).__str__())
 current_milli_time_before = int(round(time.time() * 1000))
-compressed = lz.compress(lz.get_str('sample.txt'))
+compressed = lz.compress(lz.get_bytes(file_name, True))
 current_milli_time_after = int(round(time.time() * 1000))
-print('Size in bytes after compressing : ' + lz.size_of_compressed.__str__())
+print('Size in bytes after compressing : ' + sys.getsizeof(rlp.encode(compressed)).__str__())
 print('Time taken by compressing : ' + (current_milli_time_after - current_milli_time_before).__str__() + ' ms')
-decompressed = lz.decompress(compressed)
+decompressed = (lz.decompress(compressed)).encode()
+f = open('test', 'wb')
+f.write(decompressed)
+f.close()
